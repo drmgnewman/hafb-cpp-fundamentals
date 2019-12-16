e75e13f07f2e3d18222d34f01c339dcae34d1275
Michael Newman <michaelnewman1@ad.weber.edu>
2019-12-16 09:53:09 -0700

    A cin and namespace

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "New C++ Program " << std::endl;
+    int age;    // int: signed integers
+    // cout: for system output
+    std::cout << "Now inside VSCode and github " << std::endl;
+    cout << "Another line under namespace std " << endl;
+    cout << "Wildcats " << "WSU " << "Ogden " << endl;
+ 
+    // cin: for system/user input
+    cout << "What is your age? " << endl;
+    cin >> age;
+    cout << "I see you are: " << age << endl;
     return 0;
 }
