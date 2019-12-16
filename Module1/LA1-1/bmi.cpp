f96e962d7894e52304af1e9a5a321fe712c79764
Michael Newman <michaelnewman1@ad.weber.edu>
2019-12-16 11:28:53 -0700

    Updated User Friendliness

--- Module1/LA1-1/bmi.cpp
+++ Module1/LA1-1/bmi.cpp
@@ -4,6 +4,8 @@ using namespace std;
 const float kMeterToInches = 39.37;
 const float kKiloToPound = 2.204;
 const int KBMIImperial = 703;
+const float kLowBMILimit = 18.5;
+const float kHighBMILimit = 24.9;
 
 int main()
 {
@@ -26,5 +28,17 @@ int main()
     height = height * kMeterToInches;  // convert to inches
     bmi = (weight * KBMIImperial)/height/height;
     cout << "Your BMI (imperial) is: " << bmi << endl;
+
+    // Task: Print if they are in the "good" range: BMI is in the 18.5 to 24.9
+    if(bmi > kLowBMILimit && bmi < kHighBMILimit)
+    {
+        cout << "Your BMI " << bmi << " is good" << endl;
+    }
+    else
+    {
+       cout << "Your BMI " << bmi << " is bad" << endl;
+    }
+    
+    cout << "Thank you for using the BMI program." << endl;
     return 0;
 }
