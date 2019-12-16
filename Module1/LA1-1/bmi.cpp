6fd1fcc0a3721e7e46a810f4581d2e7ae65b5135
Michael Newman <michaelnewman1@ad.weber.edu>
2019-12-16 11:16:51 -0700

    Added Imperial BMI

--- Module1/LA1-1/bmi.cpp
+++ Module1/LA1-1/bmi.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+const float kMeterToInches = 39.37;
+const float kKiloToPound = 2.204;
+const int KBMIImperial = 703;
+
 int main()
 {
     // Calculate BMI: weight(kg)/[height(m)]^2
@@ -14,5 +18,13 @@ int main()
     // calculate bmi
     bmi = weight/height/height;
     cout << "Your BMI is: " << "BMI = " << bmi << endl;
+    
+    //Task: Now, convert it to imperial units
+    // Calculate BMI (imperial): (weight(lbs) * 703)/[height(in)]^2
+    // 1 meter = 39.37 inches, 1 kg = 2.204 lbs
+    weight = weight * kKiloToPound;  // convert to lbs
+    height = height * kMeterToInches;  // convert to inches
+    bmi = (weight * KBMIImperial)/height/height;
+    cout << "Your BMI (imperial) is: " << bmi << endl;
     return 0;
 }
