839c60d2687639452dfda171c9f1955ae472d1eb
Michael Newman <michaelnewman1@ad.weber.edu>
2019-12-17 13:25:03 -0700

    initial commit

--- Module1/LA1-3/my_functions.cpp
+++ Module1/LA1-3/my_functions.cpp
@@ -1,5 +1,6 @@
 #include "my_functions.h"
 #include <iostream>
+#include<array>
 
 
 using namespace std;
@@ -23,3 +24,26 @@ void UpdateStep(int& step) // take the address of the parameter
     step += 1;
     cout << "New step: " << step << endl;
 }
+
+array<int, 10> temperatures = {-12, 34, 67, 0, 12, 44, 99, -8, 18, 33};
+
+int Max10(const std::array<int, 10>& nums)
+{
+    int max = nums[0]; // get the 
+    for(auto num : nums)
+    {
+        if(max <= num)
+        {
+            max = num;
+        }
+    }
+    return max;
+}
+
+void ClearElements(std::array<int, 10>& nums)
+{
+    for(int index = 0; index < nums.size(); ++index)
+    {
+        nums[index] = -99; // set value to -99
+    }
+} 
--- Module1/LA1-3/my_functions.h
+++ Module1/LA1-3/my_functions.h
@@ -1,4 +1,5 @@
 # pragma once
+#include <array>
 
 // prototype
 
@@ -10,3 +11,9 @@ int Sum(int num1, int num2);
  * @param step : current step value
  */
 void UpdateStep(int& step);
+
+int Max2(int num1, int num2);
+int Max3(int num1, int num2, int num3);
+int Max10(const std::array<int, 10>& nums);
+
+void ClearElements(std::array<int, 10>& nums);
