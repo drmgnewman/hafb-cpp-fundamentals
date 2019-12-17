8267b39ccc50a71e7837bf8d41d4d4db584f967e
Michael Newman <michaelnewman1@ad.weber.edu>
2019-12-17 10:50:56 -0700

    initial commit

--- Module1/LA1-3/main.cpp
+++ Module1/LA1-3/main.cpp
@@ -1,7 +1,11 @@
+#include "my_functions.h"
 #include <iostream>
+using namespace std;
 
 // Main Function
 int main()
 {
+  int number1 = 5, number2 = 10;
+  cout << "The sum of " << number1 << " and " << number2 << " is " << Sum(number1, number2) << endl;
   return 0;
 }
---
+++ Module1/LA1-3/my_functions.cpp
@@ -0,0 +1,11 @@
+#include "my_functions.h"
+#include <iostream>
+
+
+using namespace std;
+
+// Function Definition
+int Sum(int num1, int num2)
+{
+  return num1 + num2;
+}
